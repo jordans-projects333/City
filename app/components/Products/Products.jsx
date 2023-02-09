f91997a7a1fef7169a0e3db5dbbe13b9bfb8605b
Jordan <jordanroberts333@icloud.com>
2023-02-09 15:01:54 +0000

    desktop product slider

--- app/components/Products/Products.jsx
+++ app/components/Products/Products.jsx
@@ -1,9 +1,10 @@
 'use client'
 import React, { useEffect, useRef } from 'react'
 import SlideItem from './SlideItem'
-const productTitles = ['product1', 'product2', 'product3', 'product4', 'product5']
+const productTitles = ['product1', 'product2', 'product3', 'product4', 'product5', 'product6']
 
 const Products = () => {
+    let productPage = useRef(null)
     let allSlideItems = useRef([])
     let slider = useRef(null)
     let slideContainer = useRef(null)
@@ -13,6 +14,28 @@ const Products = () => {
     let animationId = useRef(null)
     let currentIndex = useRef(0)
     let startPos = useRef(0)
+    // Hover slider
+    const slideForward = () => {
+        let scrollAmount = 0
+          const slideTimer = setInterval(function(){
+            productPage.current.scrollLeft += 15;
+            scrollAmount += 15
+            if(scrollAmount > window.innerWidth){
+            //   if(productPage.current.scrollLeft === (productPage.current.scrollWidth/2)){
+                console.log('yum')
+                  window.clearInterval(slideTimer);
+              }
+          }, 1);
+        }
+    // const slideBack = () => {
+    // let slideTimer = setInterval(function(){
+    //     slider.current.scrollLeft -= 15;
+    //     if(slider.current.scrollLeft === 0){
+    //         window.clearInterval(slideTimer);
+    //     }
+    // }, 1);
+    // }
+    // Swipe slider
     function animation() {
         slider.current.style.transform = `translateX(${currentTranslation.current}px)`
         if(isDragging.current) requestAnimationFrame(animation)
@@ -55,16 +78,17 @@ const Products = () => {
             cancelAnimationFrame(animationId.current)
         }
         // disable context menu on hold
-        window.oncontextmenu = (e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            return false
-        }
+        // window.oncontextmenu = (e) => {
+        //     e.preventDefault()
+        //     e.stopPropagation()
+        //     return false
+        // }
     }, [isDragging.current])
     return (
-    <div className='lvh bg-purple-200 relative overflow-x-scroll'>
+    <div ref={productPage} className='lvh bg-purple-200 relative overflow-x-hidden md:overflow-x-auto md:bg-blue-100'>
+        <div className='hidden md:block bg-purple-400/50 absolute right-0 h-[100%] w-[20vw] top-0' onMouseOver={() => slideForward()}></div>
         <h3 className=" text-7xl whitespace-nowrap mt-4 ml-4 crab top-0">Products</h3>
-        <div ref={slideContainer} className="w-[100vw] overflow-x-hidden">
+        <div ref={slideContainer} className="w-[100vw] lg:w-[auto]">
             <div ref={slider} className='flex duration-500'>
                 {productTitles.map((object, i) => <SlideItem obj={object} key={i} index={i} refFunction={addToSlideItems} touchStart={touchStart} touchEnd={touchEnd} touchMove={touchMove}/>)}
             </div>
