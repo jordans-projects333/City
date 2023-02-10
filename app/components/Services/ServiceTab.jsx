20ca1dfa250430ae116e28cb3647af1d2379a4a1
Jordan <jordanroberts333@icloud.com>
2023-02-10 22:53:31 +0000

    fixed ontouch to onclick on servicetab to prevent trigger on scroll

--- app/components/Services/ServiceTab.jsx
+++ app/components/Services/ServiceTab.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const serviceTab = ({showcaseIn, imageRef, src, wrapperRef, treatmentType}) => {
   return (
     <div className="flex-1 lg:h-[100%] relative">
-        <div ref={wrapperRef} className="relative overflow-hidden h-full lg:h-[70%]" onTouchStart={(e) => showcaseIn(e, treatmentType)} onMouseDown={(e) => showcaseIn(e, treatmentType)}>
+        <div ref={wrapperRef} className="relative overflow-hidden h-full lg:h-[70%]" onClick={(e) => showcaseIn(e, treatmentType)}>
             <Image ref={imageRef} src={src} alt="hair care" className='absolute duration-300 left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] w-[100%] h-[150%]'/>
         </div>
         <div className='hidden lg:flex absolute bottom-0 justify-center w-full'>
