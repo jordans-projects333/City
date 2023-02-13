777c93550af83004d50cf7b9cea784fac4db0db6
Jordan <jordanroberts333@icloud.com>
2023-02-13 00:46:53 +0000

    services styles

--- app/components/Services/Hair.jsx
+++ app/components/Services/Hair.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import hairImage from '../../images/services_1.webp'
+import Image from 'next/image'
 
 const Hair = () => {
   return (
     <div>
-        <h3>hair</h3>
-        <p>Our highly trained stylists are here to tackle all of your needs, with the highest quality colouring that will leave you looking and feeling your best. Before a colouring service, we politely request that you pop in for a skin test. This will need to be done 48 hours prior to your appointment. Alternatively, you can sign a disclaimer to say you agree to your colour service without a skin test. This will only be needed for your first appoinment with us.</p>
-        <h4>What we offer</h4>
-        <h4>book appoinment</h4>
+        <div className='w-full h-[30vh] relative -z-20'>
+            <Image src={hairImage} fill alt='br' className='absolute top-0'/>
+        </div>
+        <div className='service'>
+            <div className='px-6'>
+                <h3 className='pt-4 w-full text-center font-semibold tracking-wider text-xl'>Hair Treatments</h3>
+                <div className='h-[1px] bg-black w-full my-2'></div>
+                <p className='mx-1'>Our highly trained stylists are here to tackle all of your needs, with the highest quality colouring that will leave you looking and feeling your best. Before a colouring service, we politely request that you pop in for a skin test. This will need to be done 48 hours prior to your appointment. Alternatively, you can sign a disclaimer to say you agree to your colour service without a skin test. This will only be needed for your first appoinment with us.</p>
+                <div className='w-full flex justify-center my-2 font-semibold tracking-wider text-lg'><h4>What we offer</h4></div>
+                <div className='w-full flex justify-center'><h4 className='border border-black py-2 px-4 font-semibold tracking-wider text-lg'>Book appoinment</h4></div>
+            </div>
+        </div>
     </div>
   )
 }
--- app/components/Services/Showcase.jsx
+++ app/components/Services/Showcase.jsx
@@ -28,10 +28,13 @@ const Showcase = ({showcase, slider, serviceTabsWrapper, treatments, setOnServic
     }
     return (
         <div ref={showcase} className="fixed left-[100%] top-[6vh] w-full h-[100vh] z-10 lg:absolute bg-white">
-            <div className="bg-black w-full text-white" onClick={() => showcaseOut()}>
-                back
+            <div className='service w-full bg-white flex items-center justify-center gap-2 pr-4 py-1 border-t-2 border-black' onTouchStart={() => showcaseOut()}>
+                <svg className='h-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                <h3 className='font-semibold tracking-wider text-lg'>Back</h3>
             </div>
-            {treatments}
+            <div className="">
+                {treatments}
+            </div>
         </div>
     )
   }
